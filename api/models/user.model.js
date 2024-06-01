62d2f7157cd5f96401ee387f346a980ddb67353c
vipinkarhana <vipin.karhana@gmail.com>
2024-06-02 02:25:34 +0530

    create sinhup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoos.schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
